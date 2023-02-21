d0322b5a402ea2ca9e178860cecd63918a0b9695
Hao Liu <33023091+Human9000@users.noreply.github.com>
2023-02-21 09:22:28 +0800

    Update wre_bnlved.cpp

--- model/wre_bnlved.cpp
+++ model/wre_bnlved.cpp
@@ -2,6 +2,8 @@
 extern "C" {
     void random_en_de(int B, int N, int L, bool *mask_vector, float *mae_en, float *mae_de);
     int bnl(int B, int N, int L, int b,  int n, int l);
+    void mask_en_de(int N, int L, bool *MASK, float *EN, float *DE) ;
+    int nl(int N, int L,  int n, int l);
 }
 
 #include<algorithm>
@@ -71,4 +73,58 @@ void random_en_de(int B, int N, int L, bool *mask_vector, float *mae_en, float *
             }
     }
     
+};
+
+
+int nl(int N, int L,  int n, int l) {
+    if ( n>=N | l>=L | n<0 | l<0) {
+        printf("Error(N,L,n,l):(%d,%d,%d,%d)\n",N,L,n,l);
+    }
+    return n*L + l;
+} 
+
+void mask_en_de(int N, int L, bool *MASK, float *EN, float *DE) {
+    // batch_size,select number, length
+    int l = 0, n = 0, b = 0;
+    int  p = 0, q = L - 1; // 前项指针，后向指针
+    float sum = 0;  
+
+    //处理开头，跳过开头的非选中特征
+    for (l = 0; l < L; l++)
+        if (MASK[l] == true) {
+            DE[nl(N,L,  0, l)] = 1.0;
+            EN[nl(L,N,  l, 0)] = 1.0;
+            p = l; break;
+        }
+    //处理结尾，跳过结尾的非选中特征
+    for (l = L - 1; l > -1; l--)
+        if (MASK[l] == true) {
+            DE[nl(N,L,  N-1, l)] = 1.0;
+            EN[nl(L,N, l, N-1)] = 1.0;
+            q = l; break;
+        }
+    // 处理中间，正向处理遇到选中特征后，刷新递减衰弱的距离系数
+    for (l= p + 1; l< L ; l++)
+        if (MASK[l] == true) {
+            if (++n == N) { n-=1; break; }
+            DE[nl(N,L, n, l)] = 1.0;
+            EN[nl(L,N, l, n)] = 1.0; 
+            p = l;  
+        } else if(n<N-1){
+            DE[nl(N,L, n, l)] = 1.0 / (l- p);
+        }else if(n<N){
+            DE[nl(N,L, n, l)] = 1.0;
+        }
+
+    // 处理中间，反向处理，遇到选中特征后，刷新递减衰弱的距离系数
+    for (l= q - 1; l> -1; l--)
+        if (MASK[l] == true) {
+            q = l; if (--n == -1) {  break; }
+        } else if (n > 1) {
+            sum = DE[nl(N,L, n-1, l)] + 1.0 / (q - l);
+            DE[nl(N,L, n-1, l)] /= sum;
+            DE[nl(N,L, n, l)] = 1.0 / (q - l) / sum;
+        } else if (n > 0) {
+            DE[nl(N,L, n, l)] = 1.0;
+        }
 };
